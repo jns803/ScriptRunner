a6c9a24007f9e5aae7f1c4659a685fc8a848e3d1
Jonas Stalder <jns803@hotmail.com>
2025-03-22 13:19:04 +0100

    Add unit tests for Scripts

---
+++ ScriptRunner.UnitTests/ScriptsTest.cs
@@ -0,0 +1,28 @@
+﻿using Community.PowerToys.Run.Plugin.ScriptRunner;
+using System.IO.Abstractions.TestingHelpers;
+
+namespace ScriptRunner.UnitTests
+{
+    public class ScriptsTest
+    {
+        [Fact]
+        public void Reload_InitialReload()
+        {
+            // Arrange
+            var scriptConfigs = new List<ScriptConfigDto>
+            {
+                new("name1", "path1", "dir1", "args1", "interpreter1"),
+            };
+
+            var mockFileSystem = new MockFileSystem();
+            var uut = new Scripts(mockFileSystem);
+
+            // Act
+            uut.Reload(scriptConfigs);
+            var scripts = uut.FindScripts("");
+
+            // Assert
+            scripts.Count.ShouldBe(1);
+        }
+    }
+}
