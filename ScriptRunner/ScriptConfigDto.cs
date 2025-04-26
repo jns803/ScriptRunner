3832aff737aea21d8449cf3354c7ed7e3948c0d9
Jonas Stalder <jns803@hotmail.com>
2025-04-26 13:47:43 +0200

    Document ScriptConfigDto

--- ScriptRunner/ScriptConfigDto.cs
+++ ScriptRunner/ScriptConfigDto.cs
@@ -4,15 +4,33 @@ namespace Community.PowerToys.Run.Plugin.ScriptRunner
         ScriptConfigDto[] Scripts
     );
 
+    /// <summary>
+    /// Represents the configuration for a script that can be executed by the plugin.
+    /// </summary>
+    /// <param name="Name">
+    /// The name of the script. This is used to identify the script in the configuration.
+    /// </param>
+    /// <param name="ScriptPath">
+    /// The full path to the script file.
+    /// This is required and must point to a valid script file.
+    /// </param>
+    /// <param name="WorkingDirectory">
+    /// The working directory for the script execution.
+    /// If null or whitespace, the directory where the script is located will be used.
+    /// </param>
+    /// <param name="Arguments">
+    /// The arguments to pass to the script during execution.
+    /// This can be null or empty string if no arguments are required.
+    /// </param>
+    /// <param name="Interpreter">
+    /// The interpreter to use for executing the script (e.g., Python, Bash).
+    /// If null, the plugin will attempt to infer the interpreter based on the script file type.
+    /// </param>
     public record ScriptConfigDto(
         string Name,
         string ScriptPath,
-
-        /// If WorkingDirectory is null or white space, the directory where the script is located is taken.
         string? WorkingDirectory,
-        string Arguments,
-
-        /// If Interpreter is null, the plugin tries to guess it by the file type.
+        string? Arguments,
         string? Interpreter
     );
 }
